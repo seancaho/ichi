37c155a999202d45202941101d90a9f58b967fd2
Sean Caho <seancc@proton.me>
2025-12-18 19:03:45 -0500

    Reorganize code into package and separate concerns

---
+++ ichi/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+from .input import intro, instruct, mk_heading, mk_highlight, pbcopy, \
+    capture_input, client_detection, get_client_domains
+# from .extract import extract
+# from .enrich import 
+# from .analyze import
+from .format import create_field_output, sanitize_field_output
+from .flags import get_warnings
+from .macro import get_sum_state, get_full_macro, create_meta_out
+# __all__ = ["extract"] define api...
